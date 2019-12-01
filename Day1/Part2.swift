5f7f22039b26c1a89fc9d89efc6576bedbe3ef0c
Rhys Morgan <rhysm@me.com>
2019-12-01 19:02:43 +0000

    Added Part 2 solution

---
+++ Day1/Part2.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func fuelRequirement(mass: Int) -> Int {
+	let requirement = mass / 3 - 2
+	guard requirement >= 0 else {
+		return 0
+	}
+	
+	return requirement + fuelRequirement(mass: requirement)
+}
+
+let answer = try String(contentsOfFile: "input.txt", encoding: .utf8)
+	.split(separator: "\n")
+	.compactMap { Int($0) }
+	.map(fuelRequirement)
+	.reduce(0, +)
+
+print(answer)
